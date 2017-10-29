caa4f22019a289563c4b1b3b3325745a6c0ec60b
LuckyKoala <1224393412@qq.com>
2017-10-29 22:21:33 +0800

    update Gruntfile.js so to allow cli options override

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,18 @@
 module.exports = function(grunt) {
   var config = require('./.screeps.json')
+  var branch = grunt.option('branch') || config.branch;
+  var email = grunt.option('email') || config.email;
+  var password = grunt.option('password') || config.password;
+  var ptr = grunt.option('ptr') ? true : config.ptr
+
   grunt.loadNpmTasks('grunt-screeps');
   grunt.initConfig({
       screeps: {
           options: {
-              email: config.email,
-              password: config.password,
-              branch: config.branch,
-              ptr: config.ptr
+              email: email,
+              password: password,
+              branch: branch,
+              ptr: ptr
           },
           dist: {
               src: ['src/*.js']
