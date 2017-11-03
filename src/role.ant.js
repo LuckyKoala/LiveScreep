8fca1c5e0d9e0bd7d589b362771ade4dad36be47
LuckyKoala <1224393412@qq.com>
2017-11-03 23:09:13 +0800

    prepare ant for 'Replace Older/Smaller Creep' which also need be pair with recycle action

--- src/role.ant.js
+++ src/role.ant.js
@@ -3,14 +3,23 @@ module.exports = mod;
 
 mod.Setup = {
     Normal: {
-		minEnergy: 50,
-		essBody: [MOVE],
+		minEnergy: 100,
+		essBody: [CARRY, MOVE],
 		extraBody: [],
 		prefix: '[Ant]',
 		memory: {role: 'ant'},
 	},
 };
 
+mod.roleConfig = {
+    inStack: [Action.Pickup],
+    outStack: [Action.Put],
+};
+//TODO ant can stay near spawn to collect dropped energy while other creep being recycled
 mod.loop = function(creep) {
-    //Behaviour will be control by smalltask
+	if(creep.memory.controlledByTask) {
+		//Behaviour will be control by smalltask
+	} else {
+		this.loop0(creep, creep.carry.energy == creep.carryCapacity);
+	}
 };
