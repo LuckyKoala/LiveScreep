75d1e341201bee12d2d559a51c6b5a94ec8b35d4
LuckyKoala <1224393412@qq.com>
2017-11-03 20:24:02 +0800

    add maxExtraAmount to setup of hauler since there is no need to spawn too bigger hauler

--- src/role.hauler.js
+++ src/role.hauler.js
@@ -15,6 +15,7 @@ mod.Setup = {
         //essBody: [WORK, CARRY, CARRY, CARRY, MOVE, MOVE],
         essBody: [CARRY, CARRY, CARRY, CARRY, MOVE, MOVE],
         extraBody: [CARRY, CARRY, MOVE],
+        maxExtraAmount: 3, //10Carry => 500Capacity is enough
         prefix: '[Hauler]',
         memory: {role: 'hauler'},
     },
