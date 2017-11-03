51d6579c2136ffb8bed54a82386917eec6ec1ae4
LuckyKoala <1224393412@qq.com>
2017-11-03 19:49:47 +0800

    implement initRoad function

---
+++ src/util.construction.js
@@ -0,0 +1,38 @@
+//Construction logic goes there
+var mod = {};
+module.exports = mod;
+
+//Build road
+mod.initRoad = function(room) {
+    const self = this;
+    //Spawn to Source
+    //Spawn to Controller
+    const sources = room.find(FIND_SOURCES);
+    const spawns = Util.Spawner.getSpawnsInRoom(room.name);
+    _.forEach(spawns, spawn => {
+        _.forEach(sources, source => self.buildRoad(room, spawn.pos, source.pos));
+        self.buildRoad(room, spawn.pos, room.controller.pos);
+    })
+    //Spawn to Exit
+    //const exitDir = creep.room.findExitTo(anotherCreep.room);
+    //const exit = creep.pos.findClosestByRange(exitDir);
+    //creep.moveTo(exit);
+}
+
+mod.showRoadPath = function(room, from, to) {
+    const path = room.findPath(from, to);
+    const points = _.map(path, o => [o.x, o.y]);
+    new RoomVisual(room.name).poly(points, {
+        fill: 'transparent',
+        stroke: '#fff',
+        lineStyle: 'dashed',
+        strokeWidth: .15,
+        opacity: .1
+    });
+}
+mod.buildRoad = function(room, from, to) {
+    const path = room.findPath(from, to);
+    _.forEach(path, o => room.createConstructionSite(o.x, o.y, STRUCTURE_ROAD));
+}
+
+//Layout extension
