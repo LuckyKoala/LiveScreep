eeb982a75837424f3f052caa2b8e202e4670d226
LuckyKoala <pilotcraft.lk@gmail.com>
2017-11-06 13:01:53 +0800

    refactor withdraw action

--- src/action.withdraw.js
+++ src/action.withdraw.js
@@ -1,65 +1,72 @@
 let mod = new ActionObj('Withdraw');
 module.exports = mod;
-//Should mark
-const targetInitFunc = function(creep) {
-    const role = creep.memory.role;
-    const findSuitableContainer = function(o) { 
-        if(o.structureType == STRUCTURE_CONTAINER || o.structureType == STRUCTURE_STORAGE) {
-            var need = creep.carryCapacity - creep.carry.energy;
-            if(o.store[RESOURCE_ENERGY] > need) {
-                return true;
-            }
-        }
-        return false;
-    };
-    const suitableContainers = creep.room.find(FIND_STRUCTURES, {
-        filter: findSuitableContainer
-    });
-    const controllerContainer = creep.room.controller.container;
 
-    if(role == 'hauler') {
-        //Hauler shouldn't withdraw energy from container/storage which is used by upgrader
-        // which will make hauler trapped in withdraw-transfer loop due to action.put is also
-        // used by hauler.
-        var targets = suitableContainers;
-        const controllerContainerId = controllerContainer.id;
-        if(controllerContainer) {
-            targets = _.filter(targets, o => o.id != controllerContainerId);
-        } 
-        return creep.pos.findClosestByRange(targets);
-    } else if(role == 'filler') {
-        const spawnLink = creep.room.spawnLink;
-        const sourceLink = creep.room.sourceLink;
-        if(spawnLink) {
-            //Only target spawnLink when it has enough energy or else abandon this target, try next action
-            if(sourceLink && sourceLink.energy==sourceLink.energyCapacity) {
-                //SourceLink is ready to transfer energy, so filler should move all the remain energy
-                //  in SpawnLink as soon as it can.
+function findTargetForHauler(creep, suitableContainers) {
+    //Hauler shouldn't withdraw energy from container/storage which is used by upgrader
+    // which will make hauler trapped in withdraw-transfer loop due to action.put is also
+    // used by hauler.
+    const controllerContainer = creep.room.controller.container;
+    var targets = suitableContainers;
+    const controllerContainerId = controllerContainer.id;
+    if(controllerContainer) {
+        targets = _.filter(targets, o => o.id != controllerContainerId);
+    } 
+    return creep.pos.findClosestByRange(targets);
+}
+
+function findTargetForFiller(creep) {
+    const spawnLink = creep.room.spawnLink;
+    const sourceLink = creep.room.sourceLink;
+    if(spawnLink) {
+        //Only target spawnLink when it has enough energy or else abandon this target, try next action
+        if(sourceLink && sourceLink.energy==sourceLink.energyCapacity) {
+            //SourceLink is ready to transfer energy, so filler should move all the remain energy
+            //  in SpawnLink as soon as it can.
+            creep.memory.skipBefore = 0;
+            return spawnLink;
+        } else {
+            if(spawnLink.energy > (creep.carryCapacity-creep.carry.energy)) {
                 creep.memory.skipBefore = 0;
                 return spawnLink;
             } else {
-                if(spawnLink.energy > (creep.carryCapacity-creep.carry.energy)) {
+                if(_.isUndefined(creep.memory.skipBefore)) creep.memory.skipBefore=0;
+                //If SourceLink is not ready and SpawnLink don't have enough energy, try next action once
+                //Memorize it skip spawnLink
+                const skipBefore = creep.memory.skipBefore;
+                if(skipBefore && spawnLink.energy>0) {
+                    //We have skip spawnLink at least one times, so we will target it as long
+                    //  as it has any energy
                     creep.memory.skipBefore = 0;
                     return spawnLink;
                 } else {
-                    if(_.isUndefined(creep.memory.skipBefore)) creep.memory.skipBefore=0;
-                    //If SourceLink is not ready and SpawnLink don't have enough energy, try next action once
-                    //Memorize it skip spawnLink
-                    const skipBefore = creep.memory.skipBefore;
-                    if(skipBefore && spawnLink.energy>0) {
-                        //We have skip spawnLink at least one times, so we will target it as long
-                        //  as it has any energy
-                        creep.memory.skipBefore = 0;
-                        return spawnLink;
-                    } else {
-                        creep.memory.skipBefore++;
-                        return false;
-                    }
+                    creep.memory.skipBefore++;
+                    return false;
                 }
             }
-        } else {
-            return creep.room.storage;
         }
+    } else {
+        return creep.room.storage;
+    }
+}
+
+const targetInitFunc = function(creep) {
+    const role = creep.memory.role;
+    const suitableContainers = creep.room.find(FIND_STRUCTURES, {
+        filter: function(o) { 
+            if(o.structureType == STRUCTURE_CONTAINER || o.structureType == STRUCTURE_STORAGE) {
+                var need = creep.carryCapacity - creep.carry.energy;
+                if(o.store[RESOURCE_ENERGY] > need) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    });
+    
+    if(role == 'hauler') {
+        return findTargetForHauler(creep, suitableContainers);
+    } else if(role == 'filler') {
+        return findTargetForFiller(creep);
     } else {
         return creep.pos.findClosestByRange(suitableContainers);
     }
