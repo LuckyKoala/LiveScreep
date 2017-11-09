c6269c5a5aaa0f7fd9989289412c25c2b8b2dda7
LuckyKoala <pilotcraft.lk@gmail.com>
2017-11-09 08:43:11 +0800

    not to mark target so creep can re-select closest hostile creep

--- src/action.guard.js
+++ src/action.guard.js
@@ -2,18 +2,14 @@ let mod = new ActionObj('Guard');
 module.exports = mod;
 
 mod.nextTarget = function() {
-    return Util.Mark.handleMark(this.creep, creep => {
-        const sortHostiles = Util.Defense.sortHostilesByPos(creep.room, creep.pos);
-        return sortHostiles.length ?
-        　　Game.getObjectById(_.last(sortHostiles).id) : false;
-    }, this.actionName);
+    const sortHostiles = Util.Defense.sortHostilesByPos(creep.room, creep.pos);
+    return sortHostiles.length ?
+    　　Game.getObjectById(_.last(sortHostiles).id) : false;
 };
 
 mod.loop = function(creep) {
     return this.loop0(creep, (creep, target) => {
-        if(creep.attack(target) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-        }
-        //TODO maybe destory all the attack part and then change target?
+        creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+        creep.attack(target);
     });
 };
