d54c28bea5aa7bd49a728e03dd2c40b9ded8e3aa
LuckyKoala <pilotcraft.lk@gmail.com>
2017-11-11 14:40:35 +0800

    hauler will only do feed action when there is no storage in the room

--- src/role.hauler.js
+++ src/role.hauler.js
@@ -3,10 +3,19 @@ module.exports = mod;
 
 mod.roleConfig = {
     inStack: [Action.Pickup, Action.Withdraw],
+    outStack: [Action.Fuel, Action.Put],
+};
+
+mod.roleConfigWhenNoStorage = {
+    inStack: [Action.Pickup, Action.Withdraw],
     outStack: [Action.Feed, Action.Fuel, Action.Put],
 };
 
 mod.loop = function(creep) {
+    if(!creep.room.storage) {
+        this.roleConfig = this.roleConfigWhenNoControllerContainer;
+    }
+    
     if(creep.memory.hauling && creep.carry.energy == 0) {
         creep.memory.hauling = false;
         creep.say('🔄 charge');
