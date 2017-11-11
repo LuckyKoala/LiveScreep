62e4cb847ced0e668fdfe1bc6b5213eac0d360db
LuckyKoala <pilotcraft.lk@gmail.com>
2017-11-11 17:04:26 +0800

    harvester now will try to step on container

--- src/action/harvest.js
+++ src/action/harvest.js
@@ -17,8 +17,24 @@ mod.nextTarget = function() {
 
 mod.loop = function(creep) {
     return this.loop0(creep, (creep, target) => {
-        if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(target, {visualizePathStyle: {stroke: '#ffaa00'}});
+        const source = target;
+        var container = target.container;
+        container = (container && source.pos.isNearTo(container)) ? container : false;
+        const containerNotTaken = container && container.pos.lookFor(LOOK_CREEPS).length == 0;
+
+        if(containerNotTaken) {
+            //Try stay above the container
+            if(creep.pos.isEqualTo(container)) {
+                creep.harvest(source);
+            } else {
+                creep.moveTo(container, {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        } else {
+            if(creep.pos.isNearTo(source)) {
+                creep.harvest(source);
+            } else {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
         }
     });
 };
