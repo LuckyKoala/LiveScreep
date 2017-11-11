5b57d5f229ed95c75d3941da2917cd9674823a90
LuckyKoala <pilotcraft.lk@gmail.com>
2017-11-11 17:06:17 +0800

    fixed untracked files

--- dist/role_harvester.js
+++
@@ -1,7 +0,0 @@
-var mod = new RoleObj('Harvester');
-module.exports = mod;
-
-mod.roleConfig = {
-    inStack: [Action.Harvest],
-    outStack: [Action.Put, Action.Drop], 
-};
--- dist/task_obj.js
+++
@@ -1,9 +0,0 @@
-const TaskObj = function(taskName) {
-    this.taskName = taskName;
-    //Memory Path: Memory.tasks[taskName];
-    this.memoryOf = function(taskName) {
-        if(_.isUndefined(Memory.tasks) || !_.isObject(Memory.tasks)) Memory.tasks={};
-    };
-}
-
-module.exports = TaskObj;
