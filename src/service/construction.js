09ccb64e7f9bcb7a5f1c0cf354b492a65c3ef832
LuckyKoala <pilotcraft.lk@gmail.com>
2018-12-17 21:36:40 +0800

    auto place towers and extensions; oplace a site each time

--- src/service/construction.js
+++ src/service/construction.js
@@ -2,31 +2,78 @@
 var mod = {};
 module.exports = mod;
 
+function randomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 //TODO restore mechanism
-//TODO cache in memory instead of place all sites immediately and directly
 //NOTE be careful to use findPath, it costs CPU
 mod.loop = function(room) {
-    //Init memory
-    if(_.isUndefined(Memory.construction)) Memory.construction = {};
-    if(!_.isObject(Memory.construction)) Memory.construction = {};
-    const entry = Memory.construction[room.name] || {};
-    const sources = room.find(FIND_SOURCES);
+    const sites = _.filter(Game.constructionSites, function(site) {
+        return site.room.name === room.name;
+    });
+    const noSite = sites.length === 0;
     //== Container ==
+    //Firstly, we need a container for upgrader so it won't move
     //Order here matters since only one construction site at same pile is allowed
-    //We need to init container before road.
-    if(!entry['containerInit']) {
-        this.initContainer(room, sources);
-        entry['containerInit'] = true;
+    if(noSite && !room.controller.container) {
+        this.placeSitesNearby(room, room.controller, 2, STRUCTURE_CONTAINER);
+        return;
     }
-    //== Road ==
-    if(!entry['roadInit']) {
-        this.initRoad(room, sources);
-        entry['roadInit'] = true;
+    //Then we need a container per source
+    const sources = _.filter(room.find(FIND_SOURCES), function(source) {
+        return source.container === false;
+    });
+    if(noSite && sources.length>0) {
+        this.placeSitesNearby(room, sources[0], 1, STRUCTURE_CONTAINER);
+        return;
     }
     //== Extension ==
+    //Build extension if we can
+    const extensionLimit = CONTROLLER_STRUCTURES[STRUCTURE_EXTENSION][room.controller.level];
+    var extensions = room.find(FIND_MY_STRUCTURES, {
+        filter: (structure) => {
+            return structure.structureType == STRUCTURE_EXTENSION;
+        }
+    });
+    if(noSite && extensions.length<extensionLimit) {
+        //Random selection
+        const roomArea = CONTROLLER_STRUCTURES[STRUCTURE_ROAD][0];
+        const width = Math.sqrt(roomArea);
+        const x = randomInt(width);
+        const y = randomInt(width);
+        room.createConstructionSite(x, y, STRUCTURE_EXTENSION);
+        return;
+    }
     //== Tower ==
-    //Store entry in memory
-    Memory.construction[room.name] = entry;
+    //Tower is important too !
+    const towerLimit = CONTROLLER_STRUCTURES[STRUCTURE_TOWER][room.controller.level];
+    var towers = room.find(FIND_MY_STRUCTURES, {
+        filter: (structure) => {
+            return structure.structureType == STRUCTURE_TOWER;
+        }
+    });
+    if(noSite && towers.length<towerLimit) {
+        //We can build more tower!
+        const spawns = _.filter(Game.spawns, function(spawn) {
+            return spawn.room.name === room.name;
+        });
+        this.placeSitesNearby(room, spawns[0], 4, STRUCTURE_TOWER);
+        return;
+    }
+    //== Road ==
+    if(noSite) {
+        //Init memory
+        if(_.isUndefined(Memory.construction)) Memory.construction = {};
+        if(!_.isObject(Memory.construction)) Memory.construction = {};
+        const entry = Memory.construction[room.name] || {};
+        if(!entry['roadInit']) {
+            this.initRoad(room, sources);
+            entry['roadInit'] = true;
+        }
+        //Store entry in memory
+        Memory.construction[room.name] = entry;
+    }
 };
 
 mod.initRoad = function(room, sources) {
@@ -64,41 +111,42 @@ mod.placeSitesBlock = function(room, structureType) {
     //4. place sites in position which is marked as '2'
 };
 
-mod.placeSitesNearby = function(room, objs, range, structureType) {
+mod.placeSiteAt = function(room, x, y, structureType) {
     const terrain = new Room.Terrain(room.name);
-    _.forEach(objs, obj => {
-        const y = obj.pos.y;
-        const x = obj.pos.x;
-        let swamps = [];
-        for(let xi=x-range; xi<=x+range; xi++) {
-            for(let yi=y-range; yi<=y+range; yi++) {
-                //traverse fields nearby
-                if(xi===x && yi===y) continue;
-                if(0 === terrain.get(xi, yi)) {
-                    //Plain Terrain is Best!
-                    room.createConstructionSite(xi, yi, structureType);
-                    return;
-                }
-                if(TERRAIN_MASK_SWAMP === terrain.get(xi, yi)) {
-                    //Store pos and continue finding
-                    swamps.push({x: xi, y: yi});
-                }
-            }
-        }
-        if(swamps.length >= 1) {
-            //So we must choose from swamps since there is no plain terrain as alternative
-            room.createConstructionSite(swamps[0].x, swamps[0].y, structureType);
-        }
-    });
+    if(0 === terrain.get(xi, yi)) {
+        //Plain Terrain is Best!
+        room.createConstructionSite(xi, yi, structureType);
+        return true;
+    } else {
+        return false;
+    }
 };
 
-//Random range selection, prefer plain terrain
-mod.initContainer = function(room, sources) {
-    //=== Build container near controller ===
-    this.placeSitesNearby(room, [room.controller], 2, STRUCTURE_CONTAINER);
-
-    //=== Build containers near sources ===
-    this.placeSitesNearby(room, sources, 1, STRUCTURE_CONTAINER);
+//TODO Handle not available tile, e.g a structure is already there
+mod.placeSitesNearby = function(room, obj, range, structureType) {
+    const terrain = new Room.Terrain(room.name);
+    const y = obj.pos.y;
+    const x = obj.pos.x;
+    let swamps = [];
+    for(let xi=x-range; xi<=x+range; xi++) {
+        for(let yi=y-range; yi<=y+range; yi++) {
+            //traverse fields nearby
+            if(xi===x && yi===y) continue;
+            if(0 === terrain.get(xi, yi)) {
+                //Plain Terrain is Best!
+                room.createConstructionSite(xi, yi, structureType);
+                return;
+            }
+            if(TERRAIN_MASK_SWAMP === terrain.get(xi, yi)) {
+                //Store pos and continue finding
+                swamps.push({x: xi, y: yi});
+            }
+        }
+    }
+    if(swamps.length >= 1) {
+        //So we must choose from swamps since there is no plain terrain as alternative
+        room.createConstructionSite(swamps[0].x, swamps[0].y, structureType);
+    }
 };
 
 mod.showRoadPath = function(room, from, to) {
