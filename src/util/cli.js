019287232b7d631e76f7adbe2a819ba648c2126b
LuckyKoala <pilotcraft.lk@gmail.com>
2018-12-21 15:44:44 +0800

    ccommand for open and close room plan visual

--- src/util/cli.js
+++ src/util/cli.js
@@ -1,4 +1,31 @@
-var mod = {};
 module.exports = function(cmd, args) {
-    console.log(`$(cmd) $(args)`);
+    //console.log(`$(cmd) $(args)`);
+    if(cmd === 'roomPlan') {
+        roomPlanCmdHandler(args);
+    } else {
+        notImplemented();
+    }
 };
+
+function roomPlanCmdHandler(args) {
+    //roomPlan on/off
+    if(args && args.length===2) {
+        const op = args[0];
+        const roomName = args[1];
+        if(op === 'on') {
+            Memory.rooms[roomName].showRoomPlan = true;
+        } else if(op === 'off') {
+            Memory.rooms[roomName].showRoomPlan = false;
+        }
+    } else {
+        wrongAmountOfArgs();
+    }
+};
+
+function notImplemented() {
+    console.log('command not implemented');
+};
+
+function wrongAmountOfArgs() {
+    console.log('the amount of argument is not correct');
+}
