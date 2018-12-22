85bf377aabfbb83b97c8b02ac166032d283dc98e
LuckyKoala <pilotcraft.lk@gmail.com>
2018-12-22 14:55:28 +0800

    stats on room visual

--- src/manager/room.js
+++ src/manager/room.js
@@ -50,21 +50,30 @@ mod.loopOwnedRoom = function(room) {
     QueueService.loop(room);
     SpawnService.loop(room);
     //=== Stat Display ===
+    //Game time
+    room.visual.text(`Time: ${Game.time}`, 8, 2, {color: 'white', font: 0.8});
+    //Spawn queue
+    const queue = _.union(room.queue.urgent, room.queue.normal);
+    if(queue.length) {
+        room.visual.text(`Next creep role is ${queue[0].setupName}`, 8, 3, {color: 'green', font: 0.8});
+    } else {
+        room.visual.text(`No creep in spawn queue`, 8, 3, {color: 'green', font: 0.8});
+    }
     //Energy in and out
     const lastHistory = Util.Stat.getLastHistory(room.name);
     const energyIn = lastHistory.lastAvgIn;
     const energyOut = lastHistory.lastAvgOut;
     const energyDiff = energyIn-energyOut;
-    room.visual.text(`AvgEnergy +${energyIn.toFixed(2)} -${energyOut.toFixed(2)} =${energyDiff.toFixed(2)}`, 40, 2, {color: 'green', font: 0.8});
-    //Spawn queue
-    const queue = _.union(room.queue.urgent, room.queue.normal);
-    if(queue.length) {
-        room.visual.text(`Next creep role is ${queue[0].setupName}`, 40, 3, {color: 'green', font: 0.8});
-    } else {
-        room.visual.text(`No creep in spawn queue`, 40, 3, {color: 'green', font: 0.8});
+    room.visual.text(`AvgEnergy +${energyIn.toFixed(2)} -${energyOut.toFixed(2)} = ${energyDiff.toFixed(2)}`, 8, 4, {color: 'green', font: 0.8});
+    //Controller progress
+    const progress = room.controller.progress;
+    const progressTotal = room.controller.progressTotal;
+    const percent = (progress/progressTotal).toFixed(2);
+    room.visual.text(`Progress: ${progress}/${progressTotal}(${percent})`, 8, 5, {color: 'gold', font: 0.8});
+    //Energy in storage
+    if(room.storage) {
+        room.visual.text(`Energy in storage: ${room.storage.store[RESOURCE_ENERGY]}`, 8, 6, {color: 'gold', font: 0.8});
     }
-    //Game time
-    room.visual.text(`Time: ${Game.time}`, 40, 4, {color: 'green', font: 0.8});
 };
 
 //If we have vision of unowned room, record when
