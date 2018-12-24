2fa7b123bae6f3c153c605788632956d6b61f5c8
LuckyKoala <pilotcraft.lk@gmail.com>
2018-12-24 16:17:16 +0800

    refine logic in Service.Queue

--- src/service/queue.js
+++ src/service/queue.js
@@ -21,72 +21,56 @@ mod.loop = function(room) {
         cnt[role]++;
         cnt.total++;
     }
-    //=== Base creeps ===
-    //One pair of harvester-hauler per source
-    let needHarvester = room.sources.length - cnt['Harvester'];
-    let needHauler = room.sources.length - cnt['Hauler'];
-    //Adjust queue depends on avg in and out
-    const lastHistory = Util.Stat.getLastHistory(room.name);
-    const energyIn = lastHistory.lastAvgIn;
-    const energyOut = lastHistory.lastAvgOut;
-    if(energyIn > energyOut && !room.storage) {
-        //Too many energy not used, one pair of harvester-hauler is enough
-        // That means, we don't need harvester unless no harvester at all,
-        //  but pair of hauler is required to match existed harvester
-        // But if storage is present, we have store extra energy, so we need to add check on that
-        needHarvester = 1-cnt['Harvester'];
-        needHauler = cnt['Harvester']-cnt['Hauler'];
-    }
-    while(needHarvester-- > 0) {
-        if(cnt['Harvester']===0) {
-            //No harvester at all, so it's urgent
-            room.queue.urgent.push(Setup.Harvester.setupName);
-        } else {
-            room.queue.normal.push(Setup.Harvester.setupName);
-        }
+
+    //=== Minimal group of creeps to keep room running ===
+    // At least one harvester,one hauler and one upgrader
+    if(cnt['Harvester'] < 1) {
+        room.queue.urgent.push(Setup.Harvester.setupName);
         cnt['Harvester']++;
+    }
+    if(cnt['Hauler'] < 1) {
+        room.queue.urgent.push(Setup.Hauler.setupName);
+        cnt['Hauler']++;
+    }
+    if(cnt['Upgrader'] < 1) {
+        room.queue.urgent.push(Setup.Upgrader.setupName);
+        cnt['Upgrader']++;
+    }
+    //If there is a storage, spawn a filler to redistribute
+    //  resources in storage
+    if(room.storage && cnt['Filler'] === 0) {
+        room.queue.urgent.push(Setup.Filler.setupName);
+        cnt['Filler']++;
+    }
+
+    //=== Harvest all sources and spawn dedicated hauler if no associated source link present
+    //That is one pair of harvester-hauler per source
+    // or one pair of harvester-link per source
+    let needHarvester = room.sources.length - cnt['Harvester'];
+    let needHauler = room.sources.length - room.sourceLinks.length - cnt['Hauler'];
+    //Actually enqueue harvesters and haulers
+    while(needHarvester-- > 0) {
+        room.queue.normal.push(Setup.Harvester.setupName);
+        cnt['Harvester']++;
+        //Spawn matched hauler
         if(needHauler-- > 0) {
-            if(cnt['Hauler']===0) {
-                //No hauler at all, so it's urgent
-                room.queue.urgent.push(Setup.Hauler.setupName);
-            } else {
-                room.queue.normal.push(Setup.Hauler.setupName);
-            }
+            room.queue.normal.push(Setup.Hauler.setupName);
             cnt['Hauler']++;
         }
     }
     //If harvester is enough and hauler is not enough
     // just spawn it alone
     while(needHauler-- > 0) {
-        if(cnt['Hauler']===0) {
-            //No hauler at all, so it's urgent
-            room.queue.urgent.push(Setup.Hauler.setupName);
-        } else {
-            room.queue.normal.push(Setup.Hauler.setupName);
-        }
+        room.queue.normal.push(Setup.Hauler.setupName);
+        cnt['Hauler']++;
     }
-    //One upgrader in queue.urgent and one in queue.normal
-    let upgraderCnt = cnt['Upgrader'];
-    if(upgraderCnt === 0) {
-        room.queue.urgent.push(Setup.Upgrader.setupName);
-        cnt['Upgrader']++;
-    } else if(upgraderCnt === 1) {
-        room.queue.normal.push(Setup.Upgrader.setupName);
-        cnt['Upgrader']++;
-    }
-    //=== Extended creeps ===
-    //If there is a storage, spawn a filler
-    if(room.storage) {
-        if(cnt['Filler'] === 0) {
-            room.queue.urgent.push(Setup.Filler.setupName);
-            cnt['Filler']++;
-        }
-    }
+
+    //=== Others ===
     //If there are sites need to be build or structures need to be maintain,
     //  spawn a builder for room without storage
-    //  spawn two builder from room with storage(> 15000 energy remain)
-    const needBuildStructures = room.find(FIND_CONSTRUCTION_SITES);
-    const needRepairStructures = room.find(FIND_STRUCTURES, {
+    //  spawn two builder from room with storage(> 20000 energy remain)
+    const needBuildStructures = room.cachedFind(FIND_CONSTRUCTION_SITES);
+    const needRepairStructures = room.cachedFind(FIND_STRUCTURES, {
         filter: function(o) {
             return o.hits < o.hitsMax;
         }
@@ -96,12 +80,12 @@ mod.loop = function(room) {
         if(cnt['Builder']===0) {
             room.queue.normal.push(Setup.Builder.setupName);
             cnt['Builder']++;
-        } else if(cnt['Builder']===1 && room.storage && room.storage.store[RESOURCE_ENERGY] > 15000) {
+        } else if(cnt['Builder']===1 && room.storage && room.storage.store[RESOURCE_ENERGY] > 20000) {
             room.queue.normal.push(Setup.Builder.setupName);
             cnt['Builder']++;
         }
     }
-    //If there is tower spawn a guardian -> goto util.defense
+    //Spawn a guardian if it is neccessary -> goto util.defense
     if(cnt['Guardian'] === 0 && Util.Defense.shouldSpawnGuardian(room)) {
         room.queue.urgent.push(Setup.Guardian.setupName);
         cnt['Guardian']++;
@@ -115,6 +99,10 @@ mod.loop = function(room) {
         // No storage to store extra energy or
         //   to compensate lack of energy, find a way to balance it!
         //Now let's see we have more energyIn or energyOut
+        //Adjust queue depends on avg in and out
+        const lastHistory = Util.Stat.getLastHistory(room.name);
+        const energyIn = lastHistory.lastAvgIn;
+        const energyOut = lastHistory.lastAvgOut;
         const hasMoreEnergy = (energyIn - energyOut) > 0;
         const hasLessEnergy = (energyIn - energyOut) < 0;
         console.log(`In ${energyIn}, Out ${energyOut}, hasMoreEnergy ${hasMoreEnergy}`);
