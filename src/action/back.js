6590314ff7767a28db85d7c1d1dec14be52eda59
LuckyKoala <pilotcraft.lk@gmail.com>
2018-12-25 15:56:32 +0800

    when back to homeRoom, do not stay at exit

--- src/action/back.js
+++ src/action/back.js
@@ -4,7 +4,13 @@ module.exports = mod;
 mod.nextTarget = function() {
     const creep = this.creep;
     if(creep.room.name === creep.memory.homeRoom) {
-        return false;
+        const pos = creep.pos;
+        if(pos.x === 0 || pos.x === 49 || pos.y === 0 || pos.y === 49) {
+            //Don't stay at exit, creep will loop between room
+            return creep.room.controller;
+        } else {
+            return false;
+        }
     }
 
     const homeRoom = Game.rooms[creep.memory.homeRoom];
