8fd1ddca085276ad0f1adcfdd5f8147fea3187cc
LuckyKoala <pilotcraft.lk@gmail.com>
2018-12-27 08:19:57 +0800

    if there are sites need to be build, help builder even storage present

--- src/action/help.js
+++ src/action/help.js
@@ -2,12 +2,11 @@ let mod = new ActionObj('Help');
 module.exports = mod;
 
 const targetInitFunc = function(creep) {
-    if(creep.room.storage) return false;
-
+    //if(creep.room.storage) return false;
+    const needBuildStructures = creep.room.cachedFind(FIND_CONSTRUCTION_SITES);
     const builders = _.filter(creep.room.cachedFind(FIND_MY_CREEPS), c => c.memory.role === C.BUILDER && _.sum(c.carry)<c.carryCapacity);
-    return builders.length>0 ? builders[0] : false;
+    return needBuildStructures.length>0 && builders.length>0 ? builders[0] : false;
 };
-
 mod.nextTarget = function() {
     return Util.Mark.handleMark(this.creep, targetInitFunc, this.actionName);
 };
