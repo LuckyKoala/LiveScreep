0d431c27bacd682fdbc4babf284ca341bfc8cc4b
LuckyKoala <pilotcraft.lk@gmail.com>
2018-12-31 19:13:37 +0800

    use unshift instead of push for setup.filler

--- src/task/spawning.js
+++ src/task/spawning.js
@@ -22,23 +22,21 @@ mod.queueCreeps = function(room) {
     const cnt = room.cachedRoleCount();
 
     if(room.storage) {
-        if(room.storage.store[RESOURCE_ENERGY] > 20000) {
-            //We still have energy in storage
-            // Do we have a filler to transfer energy from storage to spawns/extensions?
-            if(cnt.total[C.FILLER]>=1) {
-                //We do have a filler at least
-                // but is it bigger enough or is it will die soon?
-                //For now, we spawn two fillers, so they can help each other
-                if(cnt.total[C.FILLER]===1) {
-                    room.queue.normal.push(C.FILLER);
-                    cnt.queue[C.FILLER]++;
-                    cnt.total[C.FILLER]++;
-                }
-            } else {
-                room.queue.urgent.push(C.FILLER);
+        //We still have energy in storage
+        // Do we have a filler to transfer energy from storage to spawns/extensions?
+        if(cnt.total[C.FILLER]>=1) {
+            //We do have a filler at least
+            // but is it bigger enough or is it will die soon?
+            //For now, we spawn two fillers, so they can help each other
+            if(cnt.total[C.FILLER]===1) {
+                room.queue.normal.unshift(C.FILLER);
                 cnt.queue[C.FILLER]++;
                 cnt.total[C.FILLER]++;
             }
+        } else {
+            room.queue.urgent.unshift(C.FILLER);
+            cnt.queue[C.FILLER]++;
+            cnt.total[C.FILLER]++;
         }
     } else {
         //No storage, then it is low rcl room, task.mining will take
