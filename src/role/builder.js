d30a0caf7834b53b87530312a6bac32a61082c1f
LuckyKoala <pilotcraft.lk@gmail.com>
2019-01-22 12:43:58 +0800

    builder has two separate setupConfig now

--- src/role/builder.js
+++ src/role/builder.js
@@ -1,8 +1,13 @@
 var mod = new RoleObj(C.BUILDER);
 module.exports = mod;
 
-mod.roleConfig= {
+mod.roleConfigOutsideHome = {
     inStack: [Action.Dismantle, Action.Pickup, Action.Back, Action.FromStorage],
+    outStack: [Action.Build, Action.Repair, Action.Travel, Action.Drop], //not going home, then builder muse be doing action.dismantle, just drop energy on the ground so it can keep dismantling.
+};
+
+mod.roleConfigInHome = {
+    inStack: [Action.FromStorage, Action.Dismantle, Action.Pickup],
     //Maintain ramparts first since it will decay and it is a structure for defense
     // then repair hurt structures,roads and walls
     // build structures at last since it can wait
@@ -10,6 +15,12 @@ mod.roleConfig= {
 };
 
 mod.loop = function(creep) {
+    if(creep.memory.homeRoom === creep.room.name) {
+        this.roleConfig = this.roleConfigInHome;
+    } else {
+        this.roleConfig = this.roleConfigOutsideHome;
+    }
+
 	  if(creep.memory.building && creep.carry.energy == 0) {
 		    creep.memory.building = false;
 	  }
