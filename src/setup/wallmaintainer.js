714dc89e43ba3d1a07da2ed3616f0d6f13b3ee47
LuckyKoala <pilotcraft.lk@gmail.com>
2019-01-22 12:42:35 +0800

    bigger wallmaintainer in rcl 8

--- src/setup/wallmaintainer.js
+++ src/setup/wallmaintainer.js
@@ -13,8 +13,25 @@ mod.setupConfig = {
 		    minEnergy: 400,
 		    essBody: [CARRY, CARRY, MOVE, WORK, WORK, MOVE],
 		    extraBody: [CARRY, WORK, MOVE],
-        maxExtraAmount: 3,
+            maxExtraAmount: 3,
+		    prefix: `[${C.WALLMAINTAINER}]`,
+		    memory: {role: C.WALLMAINTAINER},
+	  },
+    High: {
+		    minEnergy: 400,
+		    essBody: [CARRY, CARRY, MOVE, WORK, WORK, MOVE],
+		    extraBody: [CARRY, WORK, MOVE],
+            maxExtraAmount: 14,
 		    prefix: `[${C.WALLMAINTAINER}]`,
 		    memory: {role: C.WALLMAINTAINER},
 	  },
 };
+
+mod.shouldUseHighLevel = function(room) {
+    return room.controller.level === 8;
+}
+};
+
+mod.shouldUseHighLevel = function(room) {
+    return room.controller.level === 8;
+}
