0252ce4a5a966459973d0f5368f80742b59ed252
LuckyKoala <pilotcraft.lk@gmail.com>
2019-01-24 21:14:16 +0800

    restore link transfer logic

--- src/service/link.js
+++ src/service/link.js
@@ -3,5 +3,38 @@ var mod = {};
 module.exports = mod;
 
 mod.loop = function(room) {
-    //
+    const inputLinks = [];
+    const outputLinks = [];
+
+    const fullAndNotCooldown = function(link) {
+        return link.energy===link.energyCapacity && link.cooldown===0;
+    };
+    for(const link of room.links) {
+        if(link.memory.marked) {
+            if(link.memory.source) {
+                if(fullAndNotCooldown(link)) inputLinks.push(link);
+            }
+            else if(link.energy===0) outputLinks.push(link);
+        } else {
+            let marked = false;
+            if(link.pos.findInRange(room.controller, 2).length > 0) {
+                link.memory.controller = true;
+                marked = true;
+            }
+            const spawnsAndExtensions = room.cachedFind(FIND_MY_STRUCTURES).filter(s => s.structureType===STRUCTURE_SPAWN || s.structureType===STRUCTURE_EXTENSION);
+            if(link.pos.findInRange(spawnsAndExtensions, 2).length > 0) {
+                link.memory.center = true;
+                marked = true;
+            }
+
+            if(!marked) link.memory.source = true;
+            link.memory.marked = true;
+        }
+    }
+
+    for(const input of inputLinks) {
+        if(outputLinks.length > 0) {
+            input.transferEnergy(outputLinks.pop());
+        }
+    }
 };
