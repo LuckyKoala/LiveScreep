8f19ac6a860ce90c2facee4968dc0335394adeab
LuckyKoala <pilotcraft.lk@gmail.com>
2019-01-25 12:49:56 +0800

    add renew mark on spawn

--- src/action/renew.js
+++ src/action/renew.js
@@ -4,7 +4,7 @@ module.exports = mod;
 mod.nextTarget = function() {
     return Util.Mark.handleMark(this.creep, creep => {
         if(creep.ticksToLive < 500) {
-            const spawns = creep.room.spawns.filter(spawn => !spawn.spawning);
+            const spawns = creep.room.spawns.filter(spawn => validateFunc(creep, spawn));
             if(spawns.length>0) return spawns[0];
         }
         return false;
@@ -13,6 +13,14 @@ mod.nextTarget = function() {
 
 const validateFunc = function(creep, target) {
     const ticksToLiveLimit = creep.getActiveBodyparts(CLAIM)>0 ? 520 : 1420;
+    const renewBlock = target.memory.renewBlock || {
+        time: 0,
+        creep: false
+    };
+    if(!_.isObject(renewBlock)) {
+        delete target.memory.renewBlock;
+    } else if(renewBlock.time===Game.time && renewBlock.creep!==creep.name) return false;
+
     return target.room && target.room.name === creep.room.name && !target.spawning && creep.ticksToLive<ticksToLiveLimit;
 };
 
@@ -24,7 +32,10 @@ mod.loop = function(creep) {
         if(result === ERR_NOT_IN_RANGE) {
             creep.moveTo(target, {range: 1, maxRooms: 1});
         } else if(result === OK) {
-            //Do nothing
+            target.memory.renewBlock = {
+                time: Game.time,
+                creep: creep.name
+            };
         }
     });
 };
