def9ae09307d2111c12e1a6498f671fd09ab9678
LuckyKoala <pilotcraft.lk@gmail.com>
2019-01-25 22:54:10 +0800

    adjust body configuration of wallmaintainer since repair_cost is 0.01

--- src/setup/wallmaintainer.js
+++ src/setup/wallmaintainer.js
@@ -3,30 +3,18 @@ module.exports = mod;
 
 mod.setupConfig = {
     Low: {
-		    minEnergy: 250,
-		    essBody: [WORK, CARRY, CARRY, MOVE],
+		    minEnergy: 300,
+		    essBody: [WORK, WORK, CARRY, MOVE],
 		    extraBody: [],
 		    prefix: `[Low-${C.WALLMAINTAINER}]`,
 		    memory: {role: C.WALLMAINTAINER},
 	  },
     Normal: {
 		    minEnergy: 400,
-		    essBody: [CARRY, CARRY, MOVE, WORK, WORK, MOVE],
-		    extraBody: [CARRY, WORK, MOVE],
-            maxExtraAmount: 3,
+		    essBody: [WORK, WORK, WORK, CARRY, MOVE],
+		    extraBody: [WORK, WORK, WORK, CARRY, MOVE],
+            maxExtraAmount: 9,
 		    prefix: `[${C.WALLMAINTAINER}]`,
 		    memory: {role: C.WALLMAINTAINER},
-	  },
-    High: {
-		    minEnergy: 400,
-		    essBody: [CARRY, CARRY, MOVE, WORK, WORK, MOVE],
-		    extraBody: [CARRY, WORK, MOVE],
-        maxExtraAmount: 14,
-		    prefix: `[${C.WALLMAINTAINER}]`,
-		    memory: {role: C.WALLMAINTAINER},
-	  },
-};
-
-mod.shouldUseHighLevel = function(room) {
-    return room.controller.level === 8;
+	  }
 };
