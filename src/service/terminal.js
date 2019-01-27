00f163e7c8764e63242e57a42857e4a939cc22c4
LuckyKoala <pilotcraft.lk@gmail.com>
2019-01-27 20:17:51 +0800

    refine market info

--- src/service/terminal.js
+++ src/service/terminal.js
@@ -3,7 +3,9 @@ module.exports = mod;
 
 //Simple:
 // sell energy/low mineral in terminal
-function ordersOf(targetRoom, resourceType, orderType, costLimit) {
+const COST_LIMIT = 600;
+const BASE_COUNT = 1000;
+function ordersOf(targetRoom, resourceType, orderType, costLimit=COST_LIMIT) {
     return Game.market.getAllOrders(order => order.resourceType === resourceType &&
                              order.type === orderType &&
                              Game.market.calcTransactionCost(1000, targetRoom, order.roomName) < costLimit);
@@ -12,8 +14,7 @@ function ordersOf(targetRoom, resourceType, orderType, costLimit) {
 //For ORDER_BUY only.
 const ENERGY_PRICE = 0.005;
 function profit(targetRoom, order) {
-    const exampleCount = 1000;
-    const cost = Game.market.calcTransactionCost(exampleCount, targetRoom, order.roomName) * ENERGY_PRICE;
+    const cost = Game.market.calcTransactionCost(BASE_COUNT, targetRoom, order.roomName) * ENERGY_PRICE;
     const profit = exampleCount*order.price;
     return profit-cost;
 }
@@ -25,9 +26,9 @@ mod.loop = function(room) {
     let sold = false;
     const sell = function(resourceType) {
         const storeAmount = room.terminal.store[resourceType];
-        
-        if(sold || storeAmount <= Config.SellExtraResourceBound || energyforCost < 600*storeAmount/1000) return;
-        const orders = _.sortBy(ordersOf(room.name, resourceType, ORDER_BUY, 600), [o => profit(room.name, o), o => o.remainingAmount]);
+
+        if(sold || storeAmount <= Config.SellExtraResourceBound || energyforCost < COST_LIMIT*storeAmount/BASE_COUNT) return;
+        const orders = _.sortBy(ordersOf(room.name, resourceType, ORDER_BUY), [o => profit(room.name, o), o => o.remainingAmount]);
         if(orders.length > 0) {
             const order = orders[0];
             let amount = Math.min(room.terminal.store[resourceType], order.remainingAmount);
@@ -35,7 +36,7 @@ mod.loop = function(room) {
                 const costAmount = Game.market.calcTransactionCost(amount, room.name, order.roomName);
                 amount -= costAmount;
             }
-            Logger.info(`[${room.name}][${order.id}]Selling ${amount} x ${resourceType} for ${order.price}`);
+            Logger.info(`[${room.name}-Market][${order.id}]Selling ${amount} x ${resourceType} for ${amount*order.price}`);
             const result = Game.market.deal(order.id, amount, room.name);
             if(result === OK) sold = true;
         }
